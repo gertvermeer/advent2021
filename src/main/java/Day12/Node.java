c0af73c5ec78b3f6269c62266245d379c25da941
Gert <gert@vermeernet.nl>
2021-12-13 08:01:36 +0100

    hhh

--- src/main/java/Day12/Node.java
+++ src/main/java/Day12/Node.java
@@ -1,6 +1,19 @@
 package Day12;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class Node {
+
+    String name;
+    List<Node> connectedNodes;
 
 
 }
---
+++ src/test/java/Day13/Day13aTest.java
@@ -0,0 +1,16 @@
+package Day13;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day13aTest {
+
+    @Test
+    void execute() {
+
+        Day13a day = new Day13a();
+
+        day.execute("13a-dev");
+    }
+}
